32aaf1b56f311a1c13f767952dde4b9c32334542
Max Laubstein <152578824+maxlaubstein@users.noreply.github.com>
2026-03-27 18:53:10 -0400

    Create pipeline.sh

---
+++ Atlapetes_Hi-C/pipeline.sh
@@ -0,0 +1,26 @@
+mamba activate chromap_env
+
+assembly="/n/holylfs06/LABS/edwards_lab/Lab/maxlaubstein/Antioquia/genomes/assembly/blancae.p_ctg.fa"
+
+#index the assembly
+chromap -i -r $assembly -o Atlapetes_index
+
+R1="/n/holylfs06/LABS/edwards_lab/Lab/maxlaubstein/Antioquia/Atlapetes_HiC/20260312_LH00541_0124_A22CTYGLT1_SUB17798/fastq/max_hi_c_S1_L001_R1_001.fastq.gz"
+R2="/n/holylfs06/LABS/edwards_lab/Lab/maxlaubstein/Antioquia/Atlapetes_HiC/20260312_LH00541_0124_A22CTYGLT1_SUB17798/fastq/max_hi_c_S1_L001_R2_001.fastq.gz"
+
+#map the Hi-C reads to the assembly. Output a SAM file
+chromap --preset hic -x Atlapetes_index  -r $assembly -1 $R1 -2 $R2 --SAM -o Ablancae.sam
+
+mamba deactivate
+mamba activate samtools_env
+#Convert SAM file to BAM file
+samtools view -bS  Ablancae.sam -o Ablancae.bam
+
+#add read groups for picard
+mamba activate picard_env
+picard AddOrReplaceReadGroups I=Ablancae.bam O=Ablancae_rg.bam RGID=1 RGLB=HiC RGPL=ILLUMINA  RGPU=unit1 RGSM=Ablancae
+
+#sort BAM file
+mamba activate samtools_env
+samtools sort -@ 16 -o Ablancae_rg_sorted.bam Ablancae_rg.bam
+
