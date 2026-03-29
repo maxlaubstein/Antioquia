d772e5944863938ec7318931011df200b7838569
Max Laubstein <152578824+maxlaubstein@users.noreply.github.com>
2026-03-29 15:21:59 -0400

    Update pipeline.sh

--- Atlapetes_Hi-C/pipeline.sh
+++ Atlapetes_Hi-C/pipeline.sh
@@ -24,3 +24,12 @@ picard AddOrReplaceReadGroups I=Ablancae.bam O=Ablancae_rg.bam RGID=1 RGLB=HiC R
 mamba activate samtools_env
 samtools sort -@ 16 -o Ablancae_rg_sorted.bam Ablancae_rg.bam
 
+#sort again by first column
+samtools sort -n -@ 16 -o Ablancae_YaHS.bam Ablancae_marked_dups.bam
+
+#index assembly
+samtools faidx $assembly
+samtools faidx /n/holylfs06/LABS/edwards_lab/Lab/maxlaubstein/Antioquia/genomes/assembly/blancae.p_ctg.fa
+
+#run YaHS:
+yahs $assembly Ablancae_YaHS.bam
